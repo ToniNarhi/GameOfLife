f8a955cc12655b411d670cf12dd5dabd38eeeb9a
somename <someemail>
2015-10-08 12:53:29 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -1,15 +1,17 @@
 package org.unioulu.tol.sqatlab.gameoflife;
+import java.util.Random;
 
 public class Grid 
 {
 	public int[][] DefineGrid()
 	{
+		Random randomGenerator = new Random();
 		int[][] grid = new int[50][50];
 		for(int x = 0; x < 50; x++)
 		{
 			for(int y = 0; y < 50; y++)
 			{			
-				grid[x][y] = 0;
+				grid[x][y] = randomGenerator.nextInt(1);
 			}
 		
 		}
