8b64f3e17ec6c5d07d0432ef7193f1aaf5244d1e
somename <someemail>
2015-10-08 13:41:02 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Main.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Main.java
@@ -1,14 +1,20 @@
 package org.unioulu.tol.sqatlab.gameoflife;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args)
 	{
 		GameOfLife GOL = new GameOfLife();
 		char quit = 'a';
-		//do
-		//{
+		
+		Scanner reader = new Scanner(System.in);
+		do
+		{
 		GOL.Logic();
-		//}while(quit != 'q');
+		
+		quit = reader.next().charAt(0);
+		}while(quit != 'q');
 	}
 }
