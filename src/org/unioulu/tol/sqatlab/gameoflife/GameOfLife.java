10105ee4653a6ce187926b72adc5f2153368373a
somename <someemail>
2015-10-08 13:13:51 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/GameOfLife.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/GameOfLife.java
@@ -31,9 +31,9 @@ public class GameOfLife {
 			{			
 				int tempNeighbors = 0;
 			
-					for(int temp_x = x-1; temp_x < x+1 && temp_x < 50 && temp_x >= -1; temp_x++)
+					for(int temp_x = x-1; temp_x < x+1 && temp_x < 50 && temp_x >= 1; temp_x++)
 					{
-						for(int temp_y = y-1; temp_y < y+1 && temp_y < 50 && temp_y >= -1; temp_y++)
+						for(int temp_y = y-1; temp_y < y+1 && temp_y < 50 && temp_y >= 1; temp_y++)
 						{				
 							if(GottenGrid[temp_x][temp_y] == 1 || temp_x != x || temp_y != y)
 							{
